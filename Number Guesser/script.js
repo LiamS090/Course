bad60e7a44ace021e0c6a82d0e58e2e5d1433516
Liam Scott <64910881+LiamS090@users.noreply.github.com>
2020-06-09 18:05:15 +0100

    Number_Guesser finished

--- Number Guesser/script.js
+++ Number Guesser/script.js
@@ -2,5 +2,23 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+  }
+  
+const compareGuesses = (userG, computerG, target) => {
+    const humanDifference = Math.abs(target - userG)
+    const computerDifference = Math.abs(target - computerG)
+    return humanDifference <= computerDifference;
+  }
+  
+const updateScore = winner => {
+    if (winner === 'human') {
+      humanScore++;
+    } else if (winner === 'computer') {
+      computerScore++;
+    }
+  }
+  
+const advanceRound = () => currentRoundNumber++;
 
