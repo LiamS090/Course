65171fce746f88c72e42957a1dec87d3f8b174a7
Liam Scott <64910881+LiamS090@users.noreply.github.com>
2020-11-05 11:37:17 +0000

    Add files via upload

--- CompanyDirectory/JS/script.js
+++ CompanyDirectory/JS/script.js
@@ -52,11 +52,18 @@ $(document).ready(function(){
           value: result.data[index].id,
           text: result.data[index].name
         }));
+        $('#departmentNewLocation').append($("<option>", {
+          value: result.data[index].id,
+          text: result.data[index].name
+        }));
+        $('#editDepartmentLocation').append($("<option>", {
+          value: result.data[index].id,
+          text: result.data[index].name
+        }));
       }); 
       loc = result['data'][0]['name'];
       locId = result['data'][0]['id'];
-      document.getElementById('locationSel').value = locId;
-      console.log(loc);
+
 
           //populating departments dropdown and editor in editor
       $.ajax({
@@ -69,14 +76,9 @@ $(document).ready(function(){
 
           console.log(result);
            
-          $.each(result.data, function(index) {
-
-            $('#departmentSel').append($("<option>", {
-              value: result.data[index].id,
-              text: result.data[index].name
-            }));                    
+          $.each(result.data, function(index) {                  
  
-            $('#Department').append($("<option>", {
+            $('#department').append($("<option>", {
               value: result.data[index].id,
               text: result.data[index].name
             }));
@@ -86,43 +88,94 @@ $(document).ready(function(){
               text: result.data[index].name
             }));
 
-            department = result['data'][0]['name'];
-            depId = result['data'][0]['id'];
-            document.getElementById('departmentSel').value = depId;
+            $('#departmentUpdate').append($("<option>", {
+              value: result.data[index].id,
+              text: result.data[index].name
+            }));
+
+
               // getting Personnel
           });
+
           $.ajax({
-            url: "PHP/getPersonelByDepartment.php",
+            url: "PHP/getDepartmentByID.php",
             type: 'POST',
             dataType: 'json',
             data: {
-              depId:depId,
+              locationsId:locId,
             },
+    
             success: function(result) {
-  
+    
               console.log(result);
-              var resultArr = result['data'];
-              resultArr.forEach(function (data) {
+               
+              $.each(result.data, function(index) {
+    
+                $('#departmentSel').append($("<option>", {
+                  value: result.data[index].id,
+                  text: result.data[index].name
+                  }));
 
-                $('#resultsTable > tbody:last-child').append("<tr class='classRow' id='"+ data.id +"'><td class='_1'>" + data.firstName + "</td><td class='_2'>" + data.lastName + "</td><td class='_3'>" + data.email + "</td><td>" + data.jobTitle + "</td></tr>");             });
-                  
-            }  
-          })
+                  department = result['data'][0]['name'];
+                  depId = result['data'][0]['id'];
+                  document.getElementById('departmentSel').value = depId;
+
+              })  
+
+              $.ajax({
+                url: "PHP/getPersonelByDepartment.php",
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                  depId:depId,
+                },
+                success: function(result) {
+  
+                console.log(result);
+                var resultArr = result['data'];
+                resultArr.forEach(function (data) {
+
+                  $('#resultsTable > tbody:last-child').append("<tr class='classRow' id='"+ data.id +"'><td class='_1'>" + data.firstName + "</td><td class='_2'>" + data.lastName + "</td><td class='_3'>" + data.email + "</td><td>" + data.jobTitle + "</td></tr>");             });               
+                }  
+              })
+            }
+          })      
         }
-      })      
+      });
     }
-  });
-});
-
+  })
+})  
     
 
    ////getting personnel when department dropdown changes but same location
 var depMenu = document.getElementById('departmentSel');
 depMenu.addEventListener('change', function(e) {
   depId = $('#departmentSel').val();
-  var locationSeleted = 'None Selected';
-  $('#locationSel').val(locationSeleted);
-   
+  
+
+    $.ajax({
+      url: "PHP/getPersonelByDepartment.php",
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        depId:depId,
+      },
+      success: function(result) {
+             
+        for(var i = table.rows.length - 1; i > 0; i--)
+       {
+          table.deleteRow(i);
+       }
+             
+        console.log(result);
+        var resultArr = result['data'];
+        resultArr.forEach(function (data) {
+               
+          $('#resultsTable > tbody:last-child').append("<tr class='classRow' id='" + data.id + "'><td class='_1'>" + data.firstName + "</td><td class='_2'>" + data.lastName + "</td><td class='_3'>" + data.email + "</td><td>" + data.jobTitle + "</td></tr>");
+        });
+      }
+    })
+     
   $.ajax({
     url: "PHP/getPersonelByDepartment.php",
     type: 'POST',
@@ -143,59 +196,95 @@ depMenu.addEventListener('change', function(e) {
              
         $('#resultsTable > tbody:last-child').append("<tr class='classRow' id='" + data.id + "'><td class='_1'>" + data.firstName + "</td><td class='_2'>" + data.lastName + "</td><td class='_3'>" + data.email + "</td><td>" + data.jobTitle + "</td></tr>");
       });
-    }  
-  })
-});
-
-//getting personel by location before department selected
-var locMenu = document.getElementById('locationSel');
-locMenu.addEventListener('change', function(e) {
-
-  locId = $('#locationSel').val();
-  var departmentSel = 'None Selected';
-  $('#departmentSel').val(departmentSel);
-
-  $.ajax({
-    url: "PHP/getDepartmentById.php",
-    type: 'POST',
-    dataType: 'json',
-    data: {
-      locId:locId,
-    },
-    success: function(result) {
-
-      console.log(result);
-
-      depId1 = result['data'][0]['id'];
-      depId2 = result['data'][1]['id'];
-
-   
-      $.ajax({
-        url: "PHP/getPersonelByLocation.php",
-        type: 'POST',
-        dataType: 'json',
-        data: {
-          depId1:depId1,
-          depId2:depId2,
-        },
-        success: function(result) {
-           
-          for(var i = table.rows.length - 1; i > 0; i--)
-          {
-            table.deleteRow(i);
-          }
-           
-          console.log(result);
-          var resultArr = result['data'];
-          resultArr.forEach(function (data) {
-             
-            $('#resultsTable > tbody:last-child').append("<tr class='classRow' id='"+ data.id +"'><td class='_1'>" + data.firstName + "</td><td class='_2'>" + data.lastName + "</td><td class='_3'>" + data.email + "</td><td>" + data.jobTitle + "</td></tr>");
-          });
-        }  
-      })
     }
   })
 });
+
+//getting personel by location
+var locMenu = document.getElementById('locationSel');
+locMenu.addEventListener('change', function(e) {
+
+  var locationsId = $('#locationSel').val();
+
+  if (locationsId == 'All') {
+    $('#departmentSel').find('option').remove();
+    document.getElementById('departmentSel').value = 'No Department selected';
+    $.ajax({
+      url: "PHP/getAllPersonel.php",
+      type: 'POST',
+      dataType: 'json',
+      data: {},
+      success: function(result) {
+
+      console.log(result);
+
+      for(var i = table.rows.length - 1; i > 0; i--)
+      {
+         table.deleteRow(i);
+      }
+
+      var resultArr = result['data'];
+      resultArr.forEach(function (data) {
+
+        $('#resultsTable > tbody:last-child').append("<tr class='classRow' id='"+ data.id +"'><td class='_1'>" + data.firstName + "</td><td class='_2'>" + data.lastName + "</td><td class='_3'>" + data.email + "</td><td>" + data.jobTitle + "</td></tr>");             });               
+      }
+    })  
+  }
+  else {
+    $.ajax({
+      url: "PHP/getDepartmentByID.php",
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        locationsId:locationsId,
+      },
+
+      success: function(result) {
+
+        console.log(result);
+
+        $('#departmentSel').find('option').remove();
+      
+        $.each(result.data, function(index) {
+
+          $('#departmentSel').append($("<option>", {
+            value: result.data[index].id,
+            text: result.data[index].name
+            }));
+            
+
+          department = result['data'][0]['name'];
+          depId = result['data'][0]['id'];
+          document.getElementById('departmentSel').value = depId;
+
+        })  
+
+        $.ajax({
+          url: "PHP/getPersonelByDepartment.php",
+          type: 'POST',
+          dataType: 'json',
+          data: {
+            depId:depId,
+          },
+          success: function(result) {
+
+          console.log(result);
+
+          for(var i = table.rows.length - 1; i > 0; i--)
+          {
+             table.deleteRow(i);
+          }
+
+          var resultArr = result['data'];
+          resultArr.forEach(function (data) {
+
+            $('#resultsTable > tbody:last-child').append("<tr class='classRow' id='"+ data.id +"'><td class='_1'>" + data.firstName + "</td><td class='_2'>" + data.lastName + "</td><td class='_3'>" + data.email + "</td><td>" + data.jobTitle + "</td></tr>");             });               
+          }  
+        })
+      }
+    })
+  }   
+})  
 
 //Displaying personnel details on click + editor
 $(document).on('click','.classRow',function(){
@@ -226,6 +315,37 @@ $(document).on('click','.classRow',function(){
     }
   })    
 });
+
+//Changing department in editor to match location selected
+var locationEdit = document.getElementById('Location');
+locationEdit.addEventListener('change', function(e) {
+
+  var locationValue = $('#Location').val();
+
+  $.ajax({
+    url: "PHP/getDepartmentByID.php",
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      locationsId:locationValue,
+    },
+
+    success: function(result) {
+
+      console.log(result);
+
+      $('#DepartmentEdit').find('option').remove();
+    
+      $.each(result.data, function(index) {
+
+        $('#DepartmentEdit').append($("<option>", {
+          value: result.data[index].id,
+          text: result.data[index].name
+        }));
+      })
+    }
+  })
+});          
 
 // updating table with new information from editor
 var updateFunc = document.getElementById('btnSubmit');
@@ -258,34 +378,43 @@ updateFunc.addEventListener('click', function(e) {
         location.href = location.href 
 })
 
+
 // deleting entry from modal after click
-var deleteFunc = document.getElementById('btnDelete');
-deleteFunc.addEventListener('click', function(e) {
-  $.ajax({
-    url: "PHP/deletePersonnel.php",
-    type: 'POST',
-    dataType: 'json',
-    data: {
-      personID: personID,
-    },
-    success: function(result) {
-      console.log(result);
-    }
-  }) 
+function ConfirmDeletePersonnel()
+{
+
+  var x = confirm("Are you sure you want to delete this personnel?");
+  if (x) {
+    $.ajax({
+      url: "PHP/deletePersonnel.php",
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        personID: personID,
+      },
+        success: function(result) {
+          console.log(result);
+        }
+    }) 
     // to refresh table
-    location.href = location.href;   
-})
- 
+    location.href = location.href;  
+    return true;
+  }   
+  else
+    return false;
+}
 
 // creating search functionailty 
 searchBtn.addEventListener('click', function () {
   var word1 = $('#searchBox').val().trim();
+  console.log(word1);
   $.ajax({
     url: "PHP/search.php",
     type: 'POST',
     dataType: 'json',
     data: {
-      name:word1,
+      name1:word1,
+      name2:word1,
     },
 
     success: function(result) {
@@ -309,12 +438,11 @@ searchBtn.addEventListener('click', function () {
 // creating new personnel
 var newPerson = document.getElementById('saveNewPerson');
 newPerson.addEventListener('click', function () {
-  var persID = document.getElementById('perID').value;
   fName = document.getElementById('firstNa').value; 
   lName = document.getElementById('lastNa').value;
   email = document.getElementById('ema').value;
   job = document.getElementById('jobT').value;
-  departID = document.getElementById('DepartmentID').value;
+  departID = document.getElementById('department').value;
 
 
   $.ajax({
@@ -322,17 +450,14 @@ newPerson.addEventListener('click', function () {
     type: 'POST',
     dataType: 'json',
     data: {
-      personID: persID,
+      depID: departID,
       fname: fName,
       lname: lName,
       email: email,
       job: job,
-      depID: departID,
     },
     success: function(result) {
       console.log(result);
-
-      
     }
   })    
       // to refresh table
@@ -344,15 +469,13 @@ var newLocation = document.getElementById('saveNewLocation');
 newLocation.addEventListener('click', function () {
 
   locaName = document.getElementById('loca').value;
-  locaID = document.getElementById('LocationID').value;
-console.log(locaID);
+
 console.log(locaName);
   $.ajax({
     url: "PHP/insertLocation.php",
     type: 'POST',
     dataType: 'json',
     data: {
-      locationID: locaID,
       name: locaName,
     },
     success: function(result) {
@@ -360,15 +483,14 @@ console.log(locaName);
     }
   })
        // to refresh table
-       location.href = location.href 
+       location.href = location.href;
 })  
 
 // creating a new department
 var newDepartment = document.getElementById('saveNewDepartment');
 newDepartment.addEventListener('click', function () {
 
-  locaID = document.getElementById('newLocationID').value;
-  departID = document.getElementById('newDepartmentID').value;
+  locaID = document.getElementById('departmentNewLocation').value;
   department = document.getElementById('depa').value;
 
   $.ajax({
@@ -377,7 +499,6 @@ newDepartment.addEventListener('click', function () {
     dataType: 'json',
     data: {
       locationID: locaID,
-      depID: departID,
       name: department,
     },
     success: function(result) {
@@ -386,22 +507,50 @@ newDepartment.addEventListener('click', function () {
 
     }
   }) 
-  location.href = location.href; 
+       // to refresh table
+       location.href = location.href; 
 })  
 
 // deleteing department
-var deleteDepart = document.getElementById('btnDeleteDepartment');
-deleteDepart.addEventListener('click', function(e) {
-
+function ConfirmDeleteDepartment()
+{
 
   department = document.getElementById('departmentDel').value;
+  var x = confirm("Are you sure you want to delete?");
+  if (x) {
+    $.ajax({
+      url: "PHP/deleteDepartmentByID.php",
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        id: department,
+      },
+        success: function(result) {
+          console.log(result);
+        }
+    }) 
+      // to refresh table
+      location.href = location.href;   
+
+      return true;
+  }    
+  else
+    return false;
+}
+// update department
+var updateDepart = document.getElementById('updateDepartment');
+updateDepart.addEventListener('click', function(e) {
+
+  department = document.getElementById('departmentUpdate').value;
+  newLocationId = document.getElementById('editDepartmentLocation').value;
 
   $.ajax({
-    url: "PHP/deleteDepartmentByID.php",
+    url: "PHP/updateDepartment.php",
     type: 'POST',
     dataType: 'json',
     data: {
       id: department,
+      locationId:newLocationId,
     },
       success: function(result) {
         console.log(result);
@@ -411,25 +560,6 @@ deleteDepart.addEventListener('click', function(e) {
     location.href = location.href;   
 })
 
-// deleteing location
-var deleteLocation = document.getElementById('btnDeleteLocation');
-deleteLocation.addEventListener('click', function(e) {
-
-  locationID = document.getElementById('locationDel').value;
-  $.ajax({
-    url: "PHP/deleteLocationByID.php",
-    type: 'POST',
-    dataType: 'json',
-    data: {
-      id: locationID,
-    },
-      success: function(result) {
-        console.log(result);
-      }
-  }) 
-    // to refresh table
-    location.href = location.href;   
-})
 
 
 
